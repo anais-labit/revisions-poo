03b9e62f411172e38a28580c7552820d9c6f113f
anais-labit <anais.labit@gmail.com>
2024-01-11 10:21:21 +0100

    job04

--- job05/index.php
+++ job05/index.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'Product.php';
 
+//  $product = new Product();
+// var_dump($result = $product->getProductWithId(7));
+
+// var_dump($product->getCategory());
+
 $product_id = 7;
 $dbConn = new PDO(
     "mysql:host=localhost;dbname=draft-shop",
@@ -13,11 +18,14 @@ $statement = $dbConn->prepare($query);
 $statement->bindParam(':product_id', $product_id);
 $statement->execute();
 $result = $statement->fetch(PDO::FETCH_ASSOC);
+// return $result ? $result : [];
 
-$photosUrl = json_decode($result['photos'], true);
+var_dump($result);
+$photosUrl = json_decode($result['photos']);
 
-$createdAt = new DateTime($result['createdAt']);
-$updatedAt = new DateTime($result['updatedAt']);
-var_dump($photoUrl, $updatedAt, $createdAt);
+$createdAt = getdate($result['createdAt']);
+$updatedAt = getdate($result['updatedAt']);
+var_dump($photosUrl, $updatedAt, $createdAt);
 
 $product = new Product($result['id'], $result['name'], $photosUrl, $result['price'], $result['description'], $result['quantity'], $createdAt, $updatedAt);
+var_dump($product);
